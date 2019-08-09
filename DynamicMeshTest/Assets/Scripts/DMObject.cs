16deead7fc61b32d7dfb16a6ff36a2569d57c488
kolenda <karolherda@gmail.com>
2019-08-10 01:17:04 +0200

    Add DMObject / DMScene.

---
+++ DynamicMeshTest/Assets/Scripts/DMObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DMObject : MonoBehaviour
+{
+	public int trianglesNum = 3;
+	public bool meshDoubleBuffering = false;
+
+	Mesh mesh = null;
+	List<Vector3>	verticesList = new List<Vector3>();
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		Debug.LogWarning( "DMObject::Start(): trianglesNum: " + trianglesNum );
+
+		if( mesh == null )
+			mesh = GetComponent<MeshFilter>().mesh;
+
+		Debug.LogWarning( "DMObject::Start(): mesh: " + mesh );
+
+		for( int i = 0; i < trianglesNum; i++ ) {
+			float angle = (float)i / trianglesNum;
+			verticesList.Add( new Vector3( Mathf.Sin(angle), Mathf.Cos( angle ), 0.0f ) );
+		}
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		mesh.Clear();
+		mesh.SetVertices( verticesList );
+//		mesh.Set
+    }
+}
---
+++ DynamicMeshTest/Assets/Scripts/DMScene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DMScene : MonoBehaviour
+{
+	public int objectsNum = 1;
+	public int trianglesNum = 1;
+
+	public GameObject	objectPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		Debug.LogWarning( "DMScene::Start(): objectsNum: " + objectsNum + ", trianglesNum: " + trianglesNum );
+
+		for( int i = 0; i < objectsNum; i++ ) {
+			Vector3 pos = new Vector3( i % 5, i / 5, 0.0f );
+			GameObject go = Instantiate( objectPrefab, pos, Quaternion.identity );
+
+			DMObject dmObject = go.GetComponent<DMObject>();
+			if( dmObject != null ) {
+				dmObject.trianglesNum = trianglesNum;
+			}
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
