6997b437401beedea7e453c5888ec94f0f97caf0
kolenda <karolherda@gmail.com>
2019-09-01 23:33:49 +0200

    New scripts.

---
+++ DynamicMeshTest/Assets/Scripts/ApplicationModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationModel
+{
+	static int objectsNum = 1;
+	public static int ObjectsNum
+	{
+		get { return objectsNum; }
+		set { objectsNum = value; }
+	}
+
+	static int triangleNum = 10;
+	public static int TriangleNum
+	{
+		get { return triangleNum; }
+		set { triangleNum = value; }
+	}
+}
---
+++ DynamicMeshTest/Assets/Scripts/ChangeSceneParam.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSceneParam : MonoBehaviour
+{
+	public void setObjectNum( int num )
+	{
+		ApplicationModel.ObjectsNum = num;
+		Debug.Log( "ChangeSceneParam.setObjectNum: " + num );
+	}
+	public void setTriangleNum( int num )
+	{
+		ApplicationModel.TriangleNum = num;
+		Debug.Log( "ChangeSceneParam.setTriangleNum: " + num );
+	}
+}
